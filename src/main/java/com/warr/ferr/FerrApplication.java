3577ec2c73b4fe16dc571aa7e548f024691825f8
한진아 <wlsdk6395@naver.com>
2024-03-05 10:40:50 +0900

    .

---
+++ src/main/java/com/warr/ferr/FerrApplication.java
@@ -0,0 +1,13 @@
+package com.warr.ferr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warr/ferr/FerrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warr.ferr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
