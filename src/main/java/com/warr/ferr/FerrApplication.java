1abfd381269b6e4c13d04714785e64008b1f5d56
M0P4RK\mosep <mose.park@aol.com>
2024-03-12 14:51:01 +0900

    Minor error fixed

--- src/main/java/com/warr/ferr/FerrApplication.java
+++ src/main/java/com/warr/ferr/FerrApplication.java
@@ -3,10 +3,8 @@ package com.warr.ferr;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.warr.ferr.*")
 @MapperScan(basePackages = "com.warr.ferr.mapper")
 public class FerrApplication {
 
