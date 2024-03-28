a4878dfe1e99c251d93729c9bb8857d6d8666ed1
M0P4RK\mosep <mose.park@aol.com>
2024-03-28 16:31:12 +0900

    Unused Imports organized

--- src/main/java/com/warr/ferr/TestController.java
+++ src/main/java/com/warr/ferr/TestController.java
@@ -1,10 +1,8 @@
 package com.warr.ferr;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
 public class TestController {
 	@GetMapping("/")
